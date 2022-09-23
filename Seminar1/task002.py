5f094aa7a6ef631583804924cc87965870c5ba48
Iliya <ilusann@inbox.ru>
2022-09-23 12:20:04 +0300

    Добавлен семинар1

---
+++ Seminar1/task002.py
@@ -0,0 +1,52 @@
+#Напишите программу, которая на вход принимает 5 чисел (можно списком) и находит максимальное из них.
+#Примеры:
+#1. 1, 4, 8, 7, 5 -> 8
+#78, 55, 36, 90, 2 -> 90
+
+#a = int(input("enter first number "))
+#b = int(input("enter second number "))
+#c = int(input("enter third number "))
+#d = int(input("enter forth number "))
+#e = int(input("enter fifth number "))
+#maxNumber = a
+#if b > maxNumber:
+#    maxNumber = b
+#if c > maxNumber:
+#    maxNumber = c
+#if d > maxNumber:
+#    maxNumber = d
+#if e > maxNumber:
+#    maxNumber = e
+#print(maxNumber)
+
+#list = [3, 6, 54, 10, 12]
+#max_dig = list1[0]
+#for i in list1:
+#    if i > max_dig:
+#        max_dig = i
+#print(max_dig)
+
+#numbers = [3, 5, 10, 7, 8]
+#print ('Максимальное число в списке', max(numbers))
+
+#entered_list = input("Введите список чисел, разделенных пробелом: ").split()
+#print("Введенный список:" ,entered_list)
+#num_list = list(map(int, entered_list))
+#print(max(num_list))
+
+#a = int(input("enter first number "))
+#b = int(input("enter second number "))
+#c = int(input("enter third number "))
+#d = int(input("enter forth number "))
+#e = int(input("enter fifth number "))
+#list_0 = [a, b, c, d , e]
+#max_number = max(list_0)
+#print("Наибольшее число:", max_number)
+
+
+
+
+
+
+
+
