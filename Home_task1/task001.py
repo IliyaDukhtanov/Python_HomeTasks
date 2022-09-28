2a83e61d6bc51a6b7109ad07d89ae5a4367b659f
Iliya <ilusann@inbox.ru>
2022-09-28 11:44:47 +0300

    Изменено условие в цикле IF для проверки нулевых координат

--- Home_task1/task001.py
+++ Home_task1/task001.py
@@ -8,3 +8,13 @@ elif a in range(6,8):
     print("Выходной :)")
 else:
     print("Рабочий")
+
+
+# Вариант 2
+#   num = int(input('Input the number of the day: '))
+# while num < 1 or num > 7:
+#   num = int(input('Wrong input. Try again here: '))
+#if num == 6 or num == 7:
+#   print('Yes')
+#else:
+#   print('No')
--- Home_task1/task005.py
+++ Home_task1/task005.py
@@ -10,5 +10,16 @@ ya = int(input("Введите координату Y точки A: "))
 xb = int(input("Введите координату X точки B: "))
 yb = int(input("Введите координату Y точки B: "))
 
-d = round(math.sqrt((xb - xa) ** 2 + (yb - ya) ** 2), 2)
+d = round(math.sqrt((xb - xa) ** 2 + (yb - ya) ** 2), 2) # можно ** 0.5 - квадратный корень
 print("Расстояние между точками A и В равно",d)
+
+
+# Вариант 2
+# ax = float(input('Enter the x coordinate of the point A: '))
+# ay = float(input('Enter the y coordinate of the point A: '))
+
+# bx = float(input('Enter the x coordinate of the point B: '))
+# by = float(input('Enter the y coordinate of the point B: '))
+
+# distance_2d = round(((bx - ax) ** 2 + (by - ay) ** 2) ** 0.5, 2)
+# print(distance_2d)
