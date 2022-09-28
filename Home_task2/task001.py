0d752304c2d801a2e908febc021e4e3044265502
Iliya <ilusann@inbox.ru>
2022-09-28 20:40:42 +0300

    Скорректирован код для подсчёта суммы через отдельную переменную

--- Home_task2/task001.py
+++ Home_task2/task001.py
@@ -3,9 +3,20 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-numb = (input("Введите число: "))  
-summa = 0
-for digit in numb:
-    if digit.isdigit():
-        summa += int(digit)
-print(f"Сумма цифр числа {numb} равна:", summa)
+#Вариант 1
+# numb = input("Введите число: ") 
+# summa = 0
+# for digit in numb:
+#     if digit.isdigit():
+#         summa += int(digit)
+#print(f"Сумма цифр числа {numb} равна:", summa)
+
+#Вариант 2
+numb = input("Введите число: ")
+n = len(numb)
+number = int(numb) * 10 ** n
+summa = 0 
+for i in range(n):
+    summa += number % 10
+    number // 10 
+    print(summa)
