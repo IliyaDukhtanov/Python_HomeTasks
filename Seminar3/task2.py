b925b9e4ac841921411a1c4b7c66e0e50f53d64a
Iliya <ilusann@inbox.ru>
2022-09-30 12:18:45 +0300

    Добавлены задачи из семинара 3 Знакомство с Python

---
+++ Seminar3/task2.py
@@ -0,0 +1,16 @@
+# Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
+
+#s = input("Введите строку: ")
+#input_list = ["q1we", "asd3", "zxc4", "qw2e", "ert5qwe"]
+input_list = [1, 3, 4, 2, 5]
+m = int(input("Введите число: "))
+#for i in input_list:
+if m in input_list:
+    print("true")
+else:
+    print("Число не входит в список")
+
+#     for digit in numb:
+#     if digit.isdigit():
+#         sum1 += int(digit)
+# print(f"Сумма цифр числа {numb} равна:", sum1)
