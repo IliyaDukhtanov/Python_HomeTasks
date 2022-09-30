3a0a6a1e5fd68f0edae1975e7e633c794f44a28c
Iliya <ilusann@inbox.ru>
2022-09-30 12:15:25 +0300

    Скорректированы имена переменных summa -> sum1

--- Home_task2/task001.py
+++ Home_task2/task001.py
@@ -4,18 +4,18 @@
 # - 0,56 -> 11
 
 #Вариант 1
-# numb = input("Введите число: ") 
-# summa = 0
-# for digit in numb:
-#     if digit.isdigit():
-#         summa += int(digit)
-# print(f"Сумма цифр числа {numb} равна:", summa)
+numb = input("Введите число: ") 
+sum1 = 0
+for digit in numb:
+    if digit.isdigit():
+        sum1 += int(digit)
+print(f"Сумма цифр числа {numb} равна:", sum1)
 
 #Вариант 2
-number = input("Введите число: ")
-n = int(float((number).replace(",", ".")) * (10 ** (len(number) - 2)))
-summa = 0 
-while n != 0:
-    summa += n % 10
-    n = n // 10 
-print(f"Сумма цифр числа {number} равна:", summa)
+# number = input("Введите число: ")
+# n = int(float((number).replace(",", ".")) * (10 ** (len(number) - 2)))
+# sum1 = 0 
+# while n != 0:
+#     sum1 += n % 10
+#     n = n // 10 
+# print(f"Сумма цифр числа {number} равна:", sum1)
