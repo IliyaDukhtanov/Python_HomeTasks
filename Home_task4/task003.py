1cbc6e4279f794de59d7742fd300806c55b8df93
Iliya <ilusann@inbox.ru>
2022-10-06 13:31:15 +0300

    Добавлено создание списка случайных чисел

--- Home_task4/task003.py
+++ Home_task4/task003.py
@@ -2,8 +2,16 @@
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
 from collections import Counter
+from random import randint
 
-input_list = list(map(int, input("Введите последовательность чисел через пробел:\n").split()))
+def create_list(k, m, n):    # создание списка случайных чисел указанного диапазона в соответствии с указанным количеством элементов
+    return [randint(m, n) for i in range(k)]
+
+
+k = int(input("Введите количество чисел в списке: "))
+m = int(input("Введите нижнюю границу чисел: "))
+n = int(input("Введите верхнюю границу чисел: "))
+input_list = create_list(k, m, n)
 output_list = [k for k, v in Counter(input_list).items() if v == 1]
 print("Исходная последовательность чисел: ", input_list)
 print("Неповторяющиеся числа: ", output_list)
