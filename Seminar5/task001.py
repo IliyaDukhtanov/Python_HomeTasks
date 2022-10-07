b72f1280eaa071d62b4ff74b4ba005a35c367cf9
Iliya <ilusann@inbox.ru>
2022-10-07 12:17:26 +0300

    Добавлена работа не семинаре №5

---
+++ Seminar5/task001.py
@@ -0,0 +1,17 @@
+# 1. В файле находится N натуральных чисел, записанных через пробел. 
+# Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число.
+
+
+def read_numbers(file):   # Получение данных из файла
+    with open(str(file), 'r') as data:
+        numbers = data.read()
+    return [int(i) for i in numbers.split()]
+
+
+file1 = "D:/Обучение/Практика/Python/Seminar5/numbers.txt"
+temp_result = read_numbers(file1)
+for i in range(1, len(temp_result)):
+    if temp_result[i - 1] != temp_result[i] - 1:
+        print(temp_result[i -1] + 1)
+
+print(temp_result)
