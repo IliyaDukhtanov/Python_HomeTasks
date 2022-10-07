19b6983c70aae392b5feae9eab17862c1cc97855
Iliya <ilusann@inbox.ru>
2022-10-07 19:27:13 +0300

    Добавлен дополнительный вариант, очень похожий

--- Home_task4/task002.py
+++ Home_task4/task002.py
@@ -1,5 +1,6 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+
 def simple_factor(m):
     factor_list = []
     d = 2 # первый делитель
@@ -15,3 +16,26 @@ def simple_factor(m):
 
 n = int(input("Введите натуральное число N: "))
 print(f"Список простых множителей для натурального числа {n}: {simple_factor(n)}")
+
+
+# Вариант однокурсницы
+
+# import math
+
+
+# def prime_factors(n):
+#     l_ist = []
+#     while n % 2 == 0:
+#         l_ist.append(2)
+#         n = n / 2   
+#     for i in range(3, int(math.sqrt(n)) + 1, 2):    # 
+#         while(n % i == 0):
+#             l_ist.append(i)
+#             n = n / i            
+#     if n > 1:
+#         l_ist.append(int(n))
+#     return l_ist
+        
+        
+# num = int(input('Enter the number N for calculation: '))
+# print(*prime_factors(num))    # знак * переводит списко в строку
