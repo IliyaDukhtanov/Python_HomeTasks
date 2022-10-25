83263877f2403e3ab09d7cfaa0f6b5d10bfaabc5
Iliya <ilusann@inbox.ru>
2022-10-25 19:27:49 +0300

    Доделана база для домашнего задания к семинару 8

--- Seminar8/contacts_op.py
+++ Seminar8/contacts_op.py
@@ -1,5 +1,6 @@
 from asyncio import create_subprocess_shell
 from dataclasses import dataclass
+from operator import concat
 from os.path import exists
 
 
@@ -16,7 +17,7 @@ def write_data(data, columns):
 
 def add_column(data, column, columns):
     for user in data: 
-        user[column] = ""
+        user[column] = "None"  # для имеющихся пользователей прописываем none в данных нового столбца, где пусто
     columns.append(column)
     return data
         
@@ -27,13 +28,10 @@ def read_data():
         return []
     with open(FILE_NAME, "r", encoding="UTF-8") as f: 
         data = f.read()
-        print(data) 
         if "\n" not in data:
             return []
-        print(data.split("\n"))
         columns = data.split("\n")[0].strip().split(", ")  # строка с заголовком 
         data = [{columns[i]: user.strip().split(", ")[i] if user else "" for i in range(len(columns))} for user in data.split("\n")[1:] if user]
-        print(data)
         return data
 
 
@@ -41,26 +39,57 @@ def get_columns(data):
     if not data: 
         return ["Фамилия", "Имя"]
     columns = list(data[0].keys())
-    print(columns)
     return columns
 
 
-def find_contact(data):
+def find_contact(data, columns):
     column = input("Введите столбец поиска: ")
     val = input("Введите значение для поиска: ")
-    # print([x for x in data if x[column] == val][0])
     flag = False
     for user in data:
-        if column not in user:
-            return "Такого столбца нет!"
+        if column not in columns:
+            return print("\nТакого столбца нет!")
         if user[column] == val: 
             print(user)
             flag = True
     if not flag: 
-        print("Данные не найдены!")
+        print("\nДанные не найдены!")
+
+
+def upd_contact(data, columns):
+    column = input("Введите столбец поиска: ")
+    val = input("Введите значение для поиска: ")
+    var = input("Введите редактируемое поле: ")
+    flag = False
+    for user in data:
+        if column not in columns:
+            return print("\nТакого столбца нет!")
+        if user[column] == val: 
+            var1 = input("Введите данные: ")
+            confirm = input("\nВыберите 1 для сохранения изменений или нажмите любую клавишу для возврата в меню: ")
+            if confirm == "1":
+                user[var] = var1
+            print(user)
+            flag = True
+            break
+    if not flag: 
+        print("\nДанные не найдены!")
+
+
+def del_contact(data, columns):
+    column = input("Введите столбец поиска: ")
+    val = input("Введите значение для поиска: ")
+    flag = False
+    for user in data:
+        if column not in columns:
+            return print("\nТакого столбца нет!")
+        elif user[column] == val:
+            confirm = input("\nВыберите 1 для сохранения изменений или нажмите любую клавишу для возврата в меню: ")
+            if confirm == "1":
+                data.remove(user)
+        flag = True
+    if not flag:
+        print("\nДанные не найдены!")
 
 
 FILE_NAME = "data_base.csv"
-# valid = exists(FILE_NAME)
-# if not valid:
-#     creat_csv() 
--- Seminar8/user_interface.py
+++ Seminar8/user_interface.py
@@ -1,26 +1,29 @@
 from contacts_op import get_columns
 from input_data import add_contact, input_column
-from contacts_op import find_contact, read_data, add_column, write_data
+from contacts_op import find_contact, read_data, add_column, write_data, upd_contact, del_contact
 
 
 def user_interface():
     data = read_data()
     columns = get_columns(data)
     flag = True
+    print("\n***** Добро пожаловать в базу данных! *****")
     while flag: 
-        print("\n***** Добро пожаловать в базу данных! *****\n\nВыберите пункт меню для продолжения:")
+        print("\nВыберите пункт меню для продолжения:")
         while True:
             print("1 - Найти контакт")
             print("2 - Добавить контакт")
             print("3 - Показать все контакты")
             print("4 - Добавить столбец")
-            print("5 - Выход")
+            print("5 - Редактировать контакт")
+            print("6 - Удалить контакт")
+            print("7 - Выход")
             choice = input()
-            if choice not in ["1", "2", "3", "4", "5"]:
+            if choice not in ["1", "2", "3", "4", "5", "6", "7"]:
                 print("!!Выбран неверный пункт меню!!\nПопробуйте ещё раз")
                 continue
             if choice == "1": 
-                find_contact(data)
+                find_contact(data, columns)
                 break
             elif choice == "2":
                 add_contact(data, columns)
@@ -28,9 +31,17 @@ def user_interface():
             elif choice == "3":
                 print(columns)
                 print(data)
+                break
             elif choice == "4":
                 column = input_column()
                 data = add_column(data, column, columns)
+                break
+            elif choice == "5":
+                upd_contact(data, columns)
+                break
+            elif choice == "6":
+                del_contact(data, columns)
+                break
             else: 
                 flag = False
                 write_data(data, columns)
