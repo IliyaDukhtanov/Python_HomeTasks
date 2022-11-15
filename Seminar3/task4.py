b36e38f48b6e82cc0af1eba96e5657f423eb7201
Iliya <ilusann@inbox.ru>
2022-11-15 18:26:56 +0300

    Добавлено решение  ДЗ семинара 11

--- Seminar3/task4.py
+++ Seminar3/task4.py
@@ -4,3 +4,16 @@ import time
 now = time.time()
 print(now)
 print(str(now).split('.')[1][0])
+
+
+#Опровержение теоремы Эйлера
+for a in range(1, 151):
+    for b in range(a,151):
+        for c in range(b, 151):
+            for d in range(c, 151):
+                sum_1 = a ** 5 + b ** 5 + c ** 5 + d ** 5
+                e = int(sum_1 ** (1/5))
+                if sum_1 == e ** 5:
+                    print(a, b, c, d, e, "сумма", a+b+c+d+e)
+
+
